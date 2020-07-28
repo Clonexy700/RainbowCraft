34148d8d25e88ef3a36efe2fa8c9b9c81a8dfefb
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-07-28 11:17:15 +0700

    little lang and light value fix

--- src/main/java/com/clonexy700/RainbowCraft/blocks/RainbowBlock.java
+++ src/main/java/com/clonexy700/RainbowCraft/blocks/RainbowBlock.java
@@ -12,7 +12,7 @@ public class RainbowBlock extends Block {
                 .hardnessAndResistance(3.0f, 6.0f)
                 .sound(SoundType.METAL)
                 .harvestLevel(2)
-                .lightValue(5)
+                .lightValue(13)
                 .harvestTool(ToolType.PICKAXE));
     }
 }
